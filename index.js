96555ddb8a19e580f56de55a481019a903b1a2eb
Youssefouah <youssefouah1997@gmail.com>
2023-09-23 15:14:22 +0100

    lol change single quotes to double one again
    Co-authored-by: wr0110 <wittyraccoon0110@gmail.com>

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-console.log('port', port);
+console.log("port", port);
 
 // Parse incoming JSON requests
 app.use(bodyParser.json());
 
 // Slack API endpoint for receiving messages
-app.post('/slack/message', async (req, res) => {
+app.post("/slack/message", async (req, res) => {
   const { type, subtype, text, channel, bot_id } = req.body.event;
 
   // Forward message to ChatGPT
-  if (!bot_id && type == 'message' && !subtype && channel == 'C051F7Y0FAL') {
-    console.log('req.body.event: ', req.body.event);
+  if (!bot_id && type == "message" && !subtype && channel == "C051F7Y0FAL") {
+    console.log("req.body.event: ", req.body.event);
     const chatGPTResponse = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
+      "https://api.openai.com/v1/chat/completions",
       {
-        model: 'gpt-3.5-turbo',
+        model: "gpt-3.5-turbo",
         temperature: 0.7,
-        messages: [{ role: 'user', content: text }],
+        messages: [{ role: "user", content: text }],
       },
       {
         headers: {
           Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       }
     );
@@ -43,19 +43,19 @@ app.post('/slack/message', async (req, res) => {
     };
 
     const response = await axios.post(
-      'https://slack.com/api/chat.postMessage',
+      "https://slack.com/api/chat.postMessage",
       message,
       {
         headers: {
           Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       }
     );
-    console.log('res', response.data);
+    console.log("res", response.data);
   }
 
-  res.status(200).send(req.body.challenge || '');
+  res.status(200).send(req.body.challenge || "");
 });
 
 app.listen(port, () => {
